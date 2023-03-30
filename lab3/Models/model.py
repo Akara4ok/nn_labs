818f5c968a4834b7edd477ca11332fcacdbc8636
Akara4ok <vladshpilka86@gmail.com>
2023-03-30 21:36:03 +0300

    lab3

---
+++ lab3/Models/model.py
@@ -0,0 +1,11 @@
+import tensorflow as tf
+
+def Model(hidden_neurons = [50, 50]):
+    model = tf.keras.Sequential()
+    model.add(tf.keras.Input(shape=(28,28)))
+    model.add(tf.keras.layers.Flatten())
+    for neurons in hidden_neurons:
+        model.add(tf.keras.layers.Dense(neurons, activation='relu'))
+    model.add(tf.keras.layers.Dense(10, activation='softmax'))
+
+    return model
