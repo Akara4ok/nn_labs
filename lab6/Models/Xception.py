8bb9bf8d065c5fddba4b9c05ed34bff06dff278c
Akara4ok <vladshpilka86@gmail.com>
2023-04-11 21:05:10 +0300

    add xception model

--- lab6/Models/Xception.py
+++
@@ -1,9 +0,0 @@
-import tensorflow as tf
-from keras import Model
-from keras.layers import Input, Dense
-
-def Xception(input_size, output_size):
-    input_layer = Input(shape=input_size)
-    output = Dense(output_size, activation='softmax')(input_layer)
-    model = Model(input_layer, output)
-    return model
